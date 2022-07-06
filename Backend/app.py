ecf6500c7322afaaec6c9b614308500a3f2c3d2b
ashirrwad <ashirwad.golu@gmail.com>
2022-07-07 00:56:27 +0530

    Initializing Flask API

--- Backend/app.py
+++ Backend/app.py
@@ -1,30 +1,61 @@
 #importing dependencies - Flask for server and docker to communicate with docker API
 from flask import Flask, jsonify, request
+from flask_cors import CORS,cross_origin
 import docker
+import os
+import tarfile
 
 app = Flask(__name__)
 
-#Editing the docker volume file with code
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#Editing the file with code inside editor
 def edit_file(code):
-  Fortran_file = open('\\\\wsl$\\docker-desktop-data\\data\\docker\\volumes\\Fortran\\_data\\Fortran.f90','w+')
+  Fortran_file = open('./File.f90','w+')
   Fortran_file.write(code)
+
+
+
+#Copying file with fortran code to container
+def copy_to(src, dst, container):
+  dst = dst
+  container = container
+
+  os.chdir(os.path.dirname(src))
+  srcname = os.path.basename(src)
+  tar = tarfile.open(src + '.tar', mode='w')
+  try:
+    tar.add(srcname)
+  finally:
+    tar.close()
+
+  data = open(src + '.tar', 'rb').read()
+  container.put_archive(os.path.dirname(dst), data)
+
 
 #Executing code inside container and getting it's output
 def execute_code_in_container():
   client = docker.from_env()
-  container = client.containers.run('sample', 'bin/bash -c "cd fortran; gfortran Fortran.f90 -o output.o; ./output.o"', volumes={'Fortran' : {'bind': '/fortran', 'mode':"rw"}},detach=True,remove=True)
-  container_object = container.attach(stdout=True, stream=True,logs=True)
-
-  for output in container_object:
-    line = output
-    return line
+  container = client.containers.run('newfort', tty=True,detach=True)
+  copy_to('./File.f90', '/fortran/File.f90',container)
+  container.exec_run('gfortran File.f90 -o executed_file.o',demux=True)
+  a = container.exec_run('./executed_file.o')
+  container.stop()
+  return a
+    
 
 #API Endpoint to submit code
-@app.route('/run', methods=['POST'])
+@app.route('/run', methods=['POST','GET','OPTIONS'])
+@cross_origin()
 def run_code():
   data = request.get_json()
   edit_file(data["code"])
   code_result = execute_code_in_container()
-  output = {"output" : code_result.decode()}
+  output = jsonify({"executed" : code_result.output.decode()})
+  return output, 202
 
-  return jsonify(output)
+if __name__ == '__main__':
+  app.run(debug = True)
+
