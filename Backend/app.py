788ffbe11aa6a0cccf5429a0419ac205a047cbb5
ashirrwad <ashirwad.golu@gmail.com>
2022-09-01 13:35:41 +0530

    Added backend for fpm library processing

--- Backend/app.py
+++ Backend/app.py
@@ -6,6 +6,8 @@ import os
 import tarfile
 import yaml
 import json
+import tomlkit
+
 app = Flask(__name__)
 
 cors = CORS(app)
@@ -14,7 +16,7 @@ app.config["CORS_HEADERS"] = "Content-Type"
 
 # Starting container
 client = docker.from_env()
-container = client.containers.run("playground-fpm", tty=True, detach=True, network_disabled=True, mem_limit="16g")
+container = client.containers.run("playground-f", tty=True, detach=True, network_disabled=True, mem_limit="16g")
 
 #Converting tutorial YAML
 with open('tutorial.yml', 'r') as file:
@@ -25,13 +27,22 @@ with open('../frontend/src/tutorial.json', 'w+') as json_file:
 
 
 # Editing the file with code inside editor
-def edit_file(code, input=""):
-    Fortran_file = open("./main.f90", "w+")
+def edit_file(code, input, libs):
+    Fortran_file = open("./main.f90", "w+") #main source code from editor
     Fortran_file.write(code)
     Fortran_file.close()
-    program_input = open("./program_input.txt", "w+")
+    program_input = open("./program_input.txt", "w+") #user input
     program_input.write(input)
     program_input.close()
+    #Generating fpm.toml for fpm processing
+    with open("fpm.toml", mode="rt", encoding="utf-8") as fp:
+        fpm = tomlkit.load(fp)
+    if "stdlib" in libs:
+        fpm["dependencies"] = {'stdlib' : {'path' : "libraries/stdlib"}}
+    else:
+        fpm["dependencies"] = {}
+    with open("fpm.toml", mode="wt", encoding="utf-8") as fp:
+        tomlkit.dump(fpm, fp)
 
 # Copying file with fortran code to container
 def copy_to(src, dst, container):
@@ -54,9 +65,9 @@ def copy_to(src, dst, container):
 def execute_code_in_container():
     copy_to('./main.f90', '/fortran/playground/app/main.f90', container)
     copy_to('./program_input.txt', '/fortran/playground/program_input.txt', container)
-
-    container.exec_run('sh -c "/fortran/fpm-0.6.0-linux-x86_64 build"')
-    a = container.exec_run('sh -c "cat program_input.txt | timeout 15s /fortran/fpm-0.6.0-linux-x86_64 run"',demux=True)
+    copy_to('./fpm.toml','/fortran/playground/fpm.toml', container)
+    container.exec_run('sh -c "/fortran/fpm build"')
+    a = container.exec_run('sh -c "cat program_input.txt | timeout 15s /fortran/fpm run"',demux=True)
 
     return a
 
@@ -66,12 +77,13 @@ def execute_code_in_container():
 @cross_origin()
 def run_code():
     data = request.get_json()
-    edit_file(data["code"], data["programInput"])
+    edit_file(data["code"], data["programInput"], data["libs"])
     code_result = execute_code_in_container()
     if code_result.output[0] == None:
         output = jsonify({"executed": ""})
         return output, 202
     output = jsonify({"executed": code_result.output[0].decode()})
+    print(code_result.output)
     
 
     return output, 202
